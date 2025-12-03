1b9befe17d7a3d4c524a2b0e7432d7ee0885d72c
EBastidas <114364709+EmilyBastidas@users.noreply.github.com>
2025-12-03 23:25:40 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -9,14 +9,15 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
-#from models import Person
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,23 +28,44 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+# listar users
+
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
+    users = User.query.all()
+    users_list = [user.serialize() for user in users]
+    return jsonify(users_list), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+#listar favoritos del user actual
 
-    return jsonify(response_body), 200
+@app.route('/user/<int:user_id>/favorites', methods=['GET'])
+def get_user_favorites(user_id):
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    favorites = [favorite.serialize() for favorite in user.favorites]
+    return jsonify(favorites), 200
+
+#agregar favoritos character o people
+@app.route('/favorite/<int:user_id>/favorites', methods=['POST'])
+def add_favorite():
+    
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
